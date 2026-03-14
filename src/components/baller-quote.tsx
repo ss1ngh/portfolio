4d993f7e86368020d0cd3c83ea83e3d3e5b88cb1
ss1ngh <singhshreyansh2354@gmail.com>
2026-03-14 23:35:39 +0530

    baller

--- src/components/baller-quote.tsx
+++ src/components/baller-quote.tsx
@@ -3,20 +3,21 @@
 import { motion } from "motion/react";
 
 export function BallQuote() {
-    return (
-        <div
-            className="absolute pointer-events-none select-none flex flex-col items-center justify-center"
-            style={{
-                left: 4000,
-                top: -1200,
-            }}
-        >
-
-            <p className="text-[7rem] font-black lowercase tracking-tighter text-[#0A0A0A]/5 select-none pointer-events-none whitespace-nowrap">
-                we rise,<br />
-                we fall,<br />
-                <span>fuck it we ball</span>
-            </p>
-        </div>
-    );
+  return (
+    <div
+      className="absolute pointer-events-none select-none flex flex-col items-center justify-center"
+      style={{
+        left: 3700,
+        top: -1000,
+      }}
+    >
+      <p className="text-[7rem] font-black lowercase tracking-wide text-[#3235F8] opacity-20 select-none pointer-events-none whitespace-nowrap">
+        we rise,
+        <br />
+        we fall,
+        <br />
+        <span>fuck it we ball</span>
+      </p>
+    </div>
+  );
 }
